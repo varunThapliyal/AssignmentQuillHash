3c321c2916ea154a88c65c5793ffd30f937e9e30
unknown <vprakashthapliyal@gmail.com>
2020-06-14 23:01:55 +0530

    Assignment Completeed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Chart from './Chart.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    lineChartData: {
+      labels: [],
+      datasets: [
+        {
+          type: "line",
+          label: "BTC-USD PRICE",
+          backgroundColor: "rgba(0,153,255,0.6)",
+          borderColor: 'rgba(0,153,255,0.2)',
+          borderWidth: "2",
+          lineTension: 0,
+          data: []
+        }
+      ]
+    },
+    lineChartOptions: {
+      responsive: true,
+      maintainAspectRatio: false,
+      tooltips: {
+        enabled: true
+      },
+      legend:{
+        display:true,
+        position:'right'
+    }
+      
+    }
+  };
+
+  componentDidMount(){
+       this.socket=new WebSocket('wss://ws-feed.pro.coinbase.com');
+       this.socket.onopen = ()=>{
+         console.log('Connection Opened');
+
+         var json = JSON.stringify({
+           "type": "subscribe",
+           "channels": [{ "name": "ticker",
+           "product_ids": ["ETH-USD"] }]
+         })
+         this.socket.send(json);
+       }
+
+       this.socket.onmessage= (event)=>{
+        const value = JSON.parse(event.data);
+
+        const previousDataSet = this.state.lineChartData.datasets[0];
+        const currentDataSet = { ...previousDataSet };
+        currentDataSet.data.push(value.price);
+
+
+
+        const latestChartData = {
+          ...this.state.lineChartData,
+          datasets: [currentDataSet],
+          labels: this.state.lineChartData.labels.concat(
+            new Date().toLocaleTimeString()
+          )
+        };
+
+        this.setState({ lineChartData: latestChartData });
+
+
+       }
+  }
+
+  componentWillUnmount() {
+    this.socket.close();
+  }
+  render(){
+    return(
+      
+      <div>BTC-USD Real Time Data
+        <Chart
+          data={this.state.lineChartData}
+          options={this.state.lineChartOptions}
+        />
+      </div>
+    )
+  }
 }
 
 export default App;
